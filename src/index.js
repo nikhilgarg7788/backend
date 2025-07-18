d6a250db84045a8a8020a7e7db961e622474480b
nikhil <ngarg7788@gmail.com>
2025-07-18 13:24:07 +0530

    files

---
+++ src/index.js
@@ -0,0 +1,54 @@
+// imported dotenv
+import dotenv from"dotenv";
+
+
+// imported mongoose below
+// import mongoose from "mongoose";
+
+// imported database below
+// import{ DB_NAME } from "./constants.js";
+
+import connectDB from "./db/index.js";
+
+
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+/*
+// imported express below
+import express from "express"
+const app = express()
+
+(async() =>{
+    try{
+        // in the below command we had connected mongodb and database
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        
+        // listeners that app uses as used beloe of error
+        app.on("error", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${
+                process.env.PORT}`);
+        })
+    }
+    catch(error){
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+    */
+   
